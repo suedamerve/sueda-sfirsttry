c4eff5b3125888e295c7d4f0210903d4e5e6fcef
suedamerve <kocasuedamerve185@gmail.com>
2024-05-25 12:41:44 +0300

    Update sueda'sfirsttry.py

--- sueda'sfirsttry.py
+++ sueda'sfirsttry.py
@@ -1,18 +1,24 @@
 import datetime
+users=[]
 
 while True:
-    kullanıcı=[]
-    Ad=input("Adınızı Giriniz:")
-    Soyad=input("Soaydınızı Giriniz:")
-    Dogum_gunu=input("Doğum yılınızı Giriniz:")
-    bugunun_tarihi=datetime.date.today()
-    yas=bugunun_tarihi.year - int(Dogum_gunu)
-    kullancıdict={"Ad": Ad,
-                  "Soyad": Soyad,
-                  "Doğum günü":Dogum_gunu,
-                  "Yaş":yas}
-    kullanıcı.append(kullancıdict)
-    print(kullanıcı)
+    name=input("Write your name: ")
+    surname=input("Write your surname: ")
+    birthday=input("What is your birthday year: ")
+    todays_date=datetime.date.today()
+    age=todays_date.year - int(birthday)
+    user={"Name":name,
+          "Surname": surname,
+          "Birthday":birthday,
+          "Age":age}
+    users.append(user)
+    print(users)
+    decision = input("To continue press Enter, To finish press q: ")
+    if decision == "q":
+        break
+    else:
+        continue
+print(users)
 
    
     
